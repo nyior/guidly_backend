73186023e00640bf1799fcad214c6b5049b26fc6
Nyior <nyior.clement@aun.edu.ng>
2024-02-08 08:29:14 +0100

    Add custome user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    slug = models.SlugField(max_length=500)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.URLField(max_length=1000)
