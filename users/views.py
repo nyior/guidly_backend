07e90d7fa0f80508446f3f2f76b91dd1e9b2c4a6
Nyior <nyior.clement@aun.edu.ng>
2024-03-02 12:59:50 +0100

    Switch lookup field for users from pk to username

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from guidly_backend.utils import generate_slug
 from users.models import CustomUser
@@ -8,6 +10,7 @@ from users.serializers import CustomUserSerializer
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    lookup_field = "username"
 
     def perform_create(self, serializer):
         name = self.request.data["first_name"] + self.request.data["last_name"]
