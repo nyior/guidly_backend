d4c002d2f9c2a2a531de436413bef73a48a56ed3
Nyior <nyior.clement@aun.edu.ng>
2024-03-03 20:24:21 +0100

    Remove unused imports

--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from guidly_backend.utils import generate_slug
 from users.models import CustomUser
