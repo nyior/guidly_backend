b11f14c76bc2131f0761cbf6d9e2c656ecab68b1
Nyior <nyior.clement@aun.edu.ng>
2024-03-12 06:51:01 +0100

    Add category fields to serializers

--- content/serializers.py
+++ content/serializers.py
@@ -15,17 +15,27 @@ class CategorySerializer(serializers.ModelSerializer):
 class GuideSerializer(serializers.ModelSerializer):
     blogs = serializers.StringRelatedField(many=True)
     authors = serializers.StringRelatedField(many=True)
+    category = serializers.StringRelatedField()
 
     class Meta:
         model = Guide
         fields = "__all__"
-        read_only_fields = ["id", "slug", "created", "updated", "views", "blogs"]
+        read_only_fields = [
+            "id",
+            "slug",
+            "created",
+            "updated",
+            "views",
+            "blogs",
+            "category",
+        ]
 
 
 class BlogSerializer(serializers.ModelSerializer):
     authors = serializers.StringRelatedField(many=True)
+    category = serializers.StringRelatedField()
 
     class Meta:
         model = Blog
         fields = "__all__"
-        read_only_fields = ["id", "slug", "created", "updated", "views"]
+        read_only_fields = ["id", "slug", "created", "updated", "views", "category"]
