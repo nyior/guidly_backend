f1baaca193473a09ed961c34342192c0868243f7
Nyior <nyior.clement@aun.edu.ng>
2024-08-20 12:52:05 +0100

    Fix bug with user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
-from users.models import CustomUser
+from users.models import ContentCreatorType, CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    creator_type = serializers.SlugRelatedField(slug_field="name")
+    creator_type = serializers.SlugRelatedField(
+        queryset=ContentCreatorType.objects.all(), slug_field="name"
+    )
 
     class Meta:
         model = CustomUser
